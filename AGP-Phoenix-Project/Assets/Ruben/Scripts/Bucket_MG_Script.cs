6384ea93f38f1ef28470d5f7a2d63736f4d3954d
RubenStender <152160273+RubenStender@users.noreply.github.com>
2026-04-09 10:13:18 +0200

    Fixed every minigame (YAAAAAAYYY)

--- AGP-Phoenix-Project/Assets/Ruben/Scripts/Bucket_MG_Script.cs
+++ AGP-Phoenix-Project/Assets/Ruben/Scripts/Bucket_MG_Script.cs
@@ -1,46 +1,67 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Bucket_MG_Script : MonoBehaviour
 {
-    public Image targetImage;
+    private string roomName = "RoomPrefab";
+    private Image targetImage;
+    private Image fireImage;
+    private Slider sliderRope;
+    private Bucket_Winch Bucket_Winch;
+    private RoomHealth roomHealth;
     public Sprite emptyBucket;
     public Sprite fullBucket;
-    public Slider sliderRope;
-    public GameObject bucketMinigame;
-    public Image fireImage;
-
     private int filledCountAmount;
     private bool bucketFilled = false;
 
-    public Bucket_Winch Winch_BMG;
-    private RoomHealth roomHealth;
-    [SerializeField] private string roomName;
+    private void Awake()
+    {
+        Bucket_Winch = GetComponentInChildren<Bucket_Winch>(true);
+        sliderRope = GetComponentInChildren<Slider>(true);
+
+        foreach (Image img in GetComponentsInChildren<Image>(true))
+        {
+            if (img.gameObject.name == "BucketImage") targetImage = img;
+            if (img.gameObject.name == "FireImage") fireImage = img;
+        }
+
+        Debug.Log("Winch: " + Bucket_Winch);
+        Debug.Log("Slider: " + sliderRope);
+        Debug.Log("TargetImage: " + targetImage);
+        Debug.Log("FireImage: " + fireImage);
+        Debug.Log("EmptyBucket: " + emptyBucket);
+        Debug.Log("FullBucket: " + fullBucket);
+    }
+
     private void Start()
     {
         fireImage.fillAmount = 1;
-        roomHealth = GameObject.Find(roomName).GetComponent<RoomHealth>();
+        roomHealth = GameObject.Find(roomName)?.GetComponent<RoomHealth>();
+        Debug.Log("RoomHealth: " + roomHealth);
     }
+
     private void Update()
     {
-        sliderRope.value = Winch_BMG.FullRotations;
+        if (Bucket_Winch == null || sliderRope == null || fireImage == null || targetImage == null) return;
+
+        sliderRope.value = Bucket_Winch.FullRotations;
+
         if (sliderRope.value == 5 && !bucketFilled)
-        {
-            Change2Full();   
-        }
+            Change2Full();
+
         if (sliderRope.value == -5 && bucketFilled)
         {
             Change2Empty();
             Debug.Log(filledCountAmount);
         }
+
         if (filledCountAmount == 5)
         {
-            roomHealth.ResetHealth(); 
-            Destroy(bucketMinigame);
-            bucketMinigame.SetActive(false);
+            roomHealth.ResetHealth();
+            gameObject.SetActive(false);
         }
     }
+
     public void Change2Empty()
     {
         filledCountAmount++;
@@ -48,6 +69,7 @@ public class Bucket_MG_Script : MonoBehaviour
         bucketFilled = false;
         targetImage.sprite = emptyBucket;
     }
+
     public void Change2Full()
     {
         bucketFilled = true;
--- AGP-Phoenix-Project/Assets/Ruben/Scripts/Bucket_Winch.cs
+++ AGP-Phoenix-Project/Assets/Ruben/Scripts/Bucket_Winch.cs
@@ -1,47 +1,77 @@
 using UnityEngine;
-public class Bucket_Winch : MonoBehaviour
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Bucket_Winch : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rectTransform;
     private Canvas canvas;
+    private Slider sliderRope;
+
     [SerializeField] private float rotationOffset = -90f;
     private float totalRotation = 0f;
     private float lastAngle = 0f;
-    private bool firstFrame = true;
+    private bool isDragging = false;
+
     public float FullRotations => totalRotation / 360f;
+
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
+        sliderRope = GetComponentInParent<Transform>().root.GetComponentInChildren<Slider>(true);
+
+        if (sliderRope != null)
+        {
+            sliderRope.minValue = -5f;
+            sliderRope.maxValue = 5f;
+            sliderRope.value = 0f;
+        }
+
+        Debug.Log("Slider found: " + sliderRope);
     }
-    void Update()
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDragging = true;
+        lastAngle = GetAngle(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!isDragging) return;
+
+        float angle = GetAngle(eventData.position);
+        float delta = Mathf.DeltaAngle(lastAngle, angle);
+        totalRotation += delta;
+        totalRotation = Mathf.Clamp(totalRotation, -1800f, 1800f);
+        lastAngle = angle;
+
+        if (sliderRope != null)
+            sliderRope.value = FullRotations;
+
+        Debug.Log($"Total Degrees: {totalRotation:F1} | Full Rotations: {FullRotations:F2}");
+
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+        rectTransform.localRotation = Quaternion.RotateTowards(
+            rectTransform.localRotation,
+            targetRotation,
+            2000f * Time.deltaTime
+        );
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDragging = false;
+    }
+
+    private float GetAngle(Vector2 pointerScreenPos)
     {
         Vector2 spriteScreenPos = RectTransformUtility.WorldToScreenPoint(
             canvas.worldCamera,
             rectTransform.position
         );
-        Vector2 direction = (Vector2)Input.mousePosition - spriteScreenPos;
-        if (direction.magnitude > 5f)
-        {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + rotationOffset;
-            if (!firstFrame)
-            {
-                float delta = Mathf.DeltaAngle(lastAngle, angle);
-                totalRotation += delta;
-                totalRotation = Mathf.Clamp(totalRotation, -1800f, 1800f);
-                Debug.Log($"Total Degrees: {totalRotation:F1} | Full Rotations: {FullRotations:F2}");
-            }
-            lastAngle = angle;
-            firstFrame = false;
-            Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
-            rectTransform.localRotation = Quaternion.RotateTowards(
-                rectTransform.localRotation,
-                targetRotation,
-                2000f * Time.deltaTime
-            );
-        }
-        else
-        {
-            firstFrame = true;
-        }
+        Vector2 direction = pointerScreenPos - spriteScreenPos;
+        return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + rotationOffset;
     }
 }
