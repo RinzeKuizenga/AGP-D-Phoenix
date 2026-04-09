aa30532a3c62c1bf5788c5c8ac67ff8594074db6
RinzeKuizenga <0355587@student.rocvantwente.nl>
2026-04-09 14:30:05 +0200

    Build fixing

--- AGP-Phoenix-Project/Assets/Daniel/Scripts/TerrainGeneration/MapGenerator.cs
+++ AGP-Phoenix-Project/Assets/Daniel/Scripts/TerrainGeneration/MapGenerator.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System;
 using System.Threading;
 using System.Collections.Generic;
-using UnityEditor.AssetImporters;
 using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
